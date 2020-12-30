4f065c1c1f9fa6db226c290f1fdc7f7596849ccb
Srichandana Bathini <srichandanabathini@srichandanasmbp.attlocal.net>
2020-12-30 18:14:04 -0500

    demo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Angular-Material
-angular material components demo
+# MaterialDemo
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.1.
+
+In this repo added few angular material components for demo
+Buttons
+Icons
+Chips
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
